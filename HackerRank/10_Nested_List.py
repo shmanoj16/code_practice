e13a63fca5901c23a2f6697c8f0271188556f6f6
Manoj <shmanoj16@gmail.com>
2023-08-23 15:59:19 -0400

    Added 10th program

--- HackerRank/10_Nested_List.py
+++ HackerRank/10_Nested_List.py
@@ -51,3 +51,15 @@
 
 # https://www.hackerrank.com/challenges/nested-list/problem?isFullScreen=true
 
+if __name__ == '__main__':
+    records=[]
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        records.append([name,score])
+    
+    second_max_grade=sorted(set([ele[1] for ele in records]))[1]
+    
+    sorted_named=sorted([items[0] for items in records if items[1] == second_max_grade])
+    for _ in sorted_named:
+        print(_)
