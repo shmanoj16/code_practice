ebdaa05d239e9e58b73527db0779065030723b9b
Manoj <shmanoj16@gmail.com>
2023-08-23 20:58:47 -0400

    3

---
+++ HackerRank/3_Arithmetic_Operators.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# Check Tutorial tab to know how to to solve.
+
+# Task
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+# Example
+
+
+# Print the following:
+
+# 8
+# -2
+# 15
+# Input Format
+
+# The first line contains the first integer, .
+# The second line contains the second integer, .
+
+# Constraints
+
+
+
+# Output Format
+
+# Print the three lines as explained above.
+
+# Sample Input 0
+
+# 3
+# 2
+# Sample Output 0
+
+# 5
+# 1
+# 6
+# Explanation 0
+
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/submissions/code/103583753
+
+if __name__ == '__main__':
+    a = int(input("Enter the value of A\t"))
+    b = int(input("Enter the value of B\t"))
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+
+
+
